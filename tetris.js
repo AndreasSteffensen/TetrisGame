8c581a25d116d18eeea68dd31736619c942868a2
AndreasSteffensen <55832629+AndreasSteffensen@users.noreply.github.com>
2024-01-03 18:14:02 +0100

    drawShape L

--- tetris.js
+++ tetris.js
@@ -4,8 +4,7 @@ var rows = 20;
 var cols = 10;
 var board;
 var context;
-var LblockX = blocksize * 2;
-var LblockY = blocksize * 2;
+var blockL = [{ x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }, { x: 3, y: 2 }];
 window.onload = function () {
     board = document.getElementById("board");
     board.height = rows * blocksize;
@@ -17,5 +16,11 @@ function update() {
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
     context.fillStyle = "green";
-    context.fillRect(LblockX, LblockY, blocksize, blocksize);
+    drawShape(blockL);
+}
+function drawShape(s) {
+    for (var _i = 0, s_1 = s; _i < s_1.length; _i++) {
+        var item = s_1[_i];
+        context.fillRect((blocksize * item.y), (blocksize * item.x), blocksize, blocksize);
+    }
 }
--- tetris.ts
+++ tetris.ts
@@ -6,9 +6,14 @@ let cols = 10;
 let board;
 let context;
 
-let LblockX = blocksize * 2;
-let LblockY = blocksize * 2;
+interface Block {
+    x: number;
+    y: number;
+}
 
+type Shape = Block[];
+
+let blockL: Shape = [{ x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }, { x: 3, y: 2 }];
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -24,5 +29,11 @@ function update() {
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle = "green";
-    context.fillRect(LblockX, LblockY, blocksize, blocksize);
+    drawShape(blockL);
+}
+
+function drawShape(s: Shape) {
+    for (var item of s) {
+        context.fillRect((blocksize * item.y),(blocksize * item.x), blocksize, blocksize);
+    }
 }
